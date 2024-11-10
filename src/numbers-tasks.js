01fd631048aa8396940dd117aa35eecb3c845150
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2024-11-10 15:44:47 +0400

    feat: update functions getRectangleArea and getCircleCircumference

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -7,34 +7,14 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-/**
- * Returns an area of a rectangle given by width and height.
- *
- * @param {number} width
- * @param {number} height
- * @return {number}
- *
- * @example:
- *   5, 10 => 50
- *   5, 5  => 25
- */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  const area = width * height;
+  return area;
 }
 
-/**
- * Returns a circumference of circle given by radius.
- *
- * @param {number} radius
- * @return {number}
- *
- * @example:
- *   5    => 31.41592653589793
- *   3.14 => 19.729201864543903
- *   0    => 0
- */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  const circumference = Math.PI * radius * 2;
+  return circumference;
 }
 
 /**
